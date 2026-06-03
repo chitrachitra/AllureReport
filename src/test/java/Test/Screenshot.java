db73aab852dc27430cb3d3ef9f9ed6a5ceb1cd2b
chitra <chitra2198682@gmail.com>
2026-06-03 00:16:17 -0500

    updated allure depency

--- src/test/java/Test/Screenshot.java
+++ src/test/java/Test/Screenshot.java
@@ -1,5 +1,17 @@
 package Test;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import io.qameta.allure.Attachment;
+
 public class Screenshot {
+	 public static WebDriver driver;
+	 @Attachment(value="failure screenshot",type="image/png")
+	 public static byte[] capturescreenshot() {
+		 return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+	 }
+	
 
 }
